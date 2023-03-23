5214d54f3bf219ff61aede28f8d615981572dbae
Patrick <patrick.acheson@hotmail.com>
2023-03-23 21:05:32 +0000

    added cv pdf viewer and cleaned up scripts

--- script.js
+++ script.js
@@ -31,10 +31,20 @@ window.addEventListener('scroll', () => {
   
   lastScrollPosition = currentScrollPosition;
 
-  //const icon = document.querySelector('.icon-scroll');
-    //icon.style.display = 'none';
+  const icon = document.querySelector('.icon-scroll');
+  hideElementForFiveSeconds(icon);
 
 });
+
+
+function hideElementForFiveSeconds(element) {
+    element.style.display = 'none';
+
+    setTimeout(() => {
+      element.style.display = 'block';
+    }, 3000);
+  }
+
 
 const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
 
@@ -76,3 +86,64 @@ balls.forEach((el, i, ra) => {
       }
     );
   });
+
+
+  const resumeButton = document.getElementById('resume-button');
+  const modalContainer = document.getElementById('modal-container');
+  
+  // Add event listener to resume button
+  resumeButton.addEventListener('click', () => {
+    // Show modal
+    modalContainer.style.display = 'block';
+  });
+  
+  // Add event listener to close modal when clicked outside modal content
+  modalContainer.addEventListener('click', (event) => {
+    if (event.target === modalContainer) {
+      modalContainer.style.display = 'none';
+    }
+  });
+
+
+// cards section one
+
+const services = [
+    {
+      title: "Scripting",
+      icon: "./icons/backend.png"
+    },
+    {
+      title: "Pentesting",
+      icon: "./icons/creator.png"
+    },
+    {
+      title: "Networking",
+      icon: "./icons/mobile.png"
+    }
+  ];
+  
+  function createServiceCard(service) {
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('service-card');
+  
+    const card = `
+      <div>
+        <div>
+          <img src="${service.icon}" alt="${service.title}"/>
+          <h3>${service.title}</h3>
+        </div>
+      </div>
+    `;
+    cardContainer.innerHTML = card;
+    return cardContainer;
+  }
+  
+  function addServiceCards() {
+    const servicesContainer = document.querySelector('#services-container');
+    services.forEach(service => {
+      const card = createServiceCard(service);
+      servicesContainer.appendChild(card);
+    });
+  }
+  
+  addServiceCards();
